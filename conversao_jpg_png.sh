9614cfa1493661c9d00dba9f3e0b3fe0ee94ba29
wagnerfns <wagnerfelidre@gmail.com>
2018-12-29 11:36:40 -0300

    Verifica se existe pasta png se não cria uma pasta

--- conversao_jpg_png.sh
+++ conversao_jpg_png.sh
@@ -2,6 +2,18 @@
 
 cd ~/Downloads/imagens-livros
 
+sleep 3
+
+echo 'Verificando se existe pasta png, se nao cria uma'
+sleep 3
+
+if [ ! -d png ]
+then
+	mkdir png
+fi
+
+echo 'Convertendo imagens .jpg para .png'
+sleep 3
 
 for imagem in *.jpg
 do 
